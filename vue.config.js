2448c78a67358f4174451ed57dd6c7f2d25cb0b2
Lasse27 <120169365+Lasse27@users.noreply.github.com>
2025-08-30 21:08:44 +0200

    Anpassungen der Electron Build Parameter -> Logo, Target Arches, App-Data

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,53 @@
 const { defineConfig } = require('@vue/cli-service')
+const { publish } = require('electron-builder')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: './',
-  pluginOptions: {
+    transpileDependencies: true,
+    publicPath: './',
+    pluginOptions: {
         electronBuilder: {
             nodeIntegration: true,
-            mainProcessFile: 'electron-main.js'
+            mainProcessFile: 'electron-main.js',
+            builderOptions: {
+                productName: "ZenTimer",
+                appId: "de.lasse27.zen-timer",
+                directories: {
+                    output: 'dist_electron'
+                },
+                files: [
+                    "**/*"
+                ],
+                icon: 'public/logo.png',
+                extraResources: [
+                    {
+                        from: "src/assets/sounds/",
+                        to: "assets/sounds/",
+                        filter: ["**/*"]
+                    }
+                ],
+                win: {
+                    target: [
+                        {
+                            target: "nsis",
+                            arch: [
+                                "x64",
+                                "ia32"
+                            ]
+                        }
+                    ]
+                },
+                nsis: {
+                    oneClick: false,
+                    allowToChangeInstallationDirectory: true
+                },
+                linux: {
+                    target: [
+                        "AppImage",
+                        "deb"
+                    ],
+                    category: "Utility"
+                },
+                publish: null
+            },
         }
     }
 })
